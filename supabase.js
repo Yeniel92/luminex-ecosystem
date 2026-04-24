331bd734061635e059f454f50b360db1d68a36da
Yeniel92 <yeniel.rodriguez92@gmail.com>
2026-04-24 14:27:31 -0400

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-// ══════════════════════════════════════════
-//  Luminex Ecosystem — Configuración Supabase
-// ══════════════════════════════════════════
-
-export const SB_URL = 'https://zsalcpmeagwxdpengwtg.supabase.co';
-export const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzYWxjcG1lYWd3eGRwZW5nd3RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2MTkzNDQsImV4cCI6MjA4ODE5NTM0NH0.MikKIKaO-fmomGosVG336xl9qPof-e0XPfuWALLyarc';
